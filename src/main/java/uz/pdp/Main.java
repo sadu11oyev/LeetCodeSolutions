9c6a9a1c297a519128ef4b7b0d549871ceba05bd
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-07-22 11:07:57 +0500

    easy issues solutions add 4

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -535,6 +535,16 @@ public class Main {
         return product - sum;
     }
 
+    //2652. Sum Multiples
+    public int sumOfMultiples(int n) {
+        int res = 0;
+        for(int i=1; i<=n; i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                res+=i;
+            }
+        }
+        return res;
+    }
 
 
 
