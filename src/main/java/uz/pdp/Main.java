0ab6cfaab505e83ba4b3ec7fb7809ef8f428827d
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-07-22 11:13:17 +0500

    easy issues solutions add 5

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -546,6 +546,22 @@ public class Main {
         return res;
     }
 
+    //1342. Number of Steps to Reduce a Number to Zero
+    public int numberOfSteps(int num) {
+        int step = 0;
+        while(num>0){
+            if(num%2==0){
+                num=num/2;
+            }else{
+                num--;
+            }
+            step++;
+        }
+        return step;
+    }
+
+
+
 
 
 }
