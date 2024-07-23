ae54dc6eb3cf00a2cf09503be4b8aea66aef8fb9
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-07-23 12:12:28 +0500

    easy issues 1662 solutions add

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -608,7 +608,19 @@ public class Main {
             }
         }
         return -1;
+    }
 
+    //1662. Check If Two String Arrays are Equivalent
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder s1 = new StringBuilder();
+        for(String word:word1){
+            s1.append(word);
+        }
+        StringBuilder s2 = new StringBuilder();
+        for(String word:word2){
+            s2.append(word);
+        }
+        return s1.toString().equals(s2.toString());
     }
 
 
