8369f26a1733f3d80c831d82dc63da2ac037e91c
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-08-07 12:20:13 +0500

    medium issues 50 solution add

--- src/main/java/uz/pdp/Medium.java
+++ src/main/java/uz/pdp/Medium.java
@@ -132,6 +132,32 @@ public class Medium {
         return ""+bigInt1.multiply(bigInt2);
     }
 
+    //50. Pow(x, n)
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return powHelper(x, n);
+    }
+
+    private static double powHelper(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double half = powHelper(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
 
 
 }
