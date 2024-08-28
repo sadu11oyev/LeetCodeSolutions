6f0017462ac31670e82105f984318aca7ced3412
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-08-28 10:55:33 +0500

    easy issues solutions add 3

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -523,6 +523,19 @@ public class Main {
         }
     }
 
+    //1281. Subtract the Product and Sum of Digits of an Integer
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        while(n>0){
+            product*=(n%10);
+            sum+=n%10;
+            n=n/10;
+        }
+        return product - sum;
+    }
+
+
 
 
 }
