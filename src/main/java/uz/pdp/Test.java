51db0a209b853c84a3a35ea2604e101aefb60ca6
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-09-01 16:54:34 +0500

    add Test class

--- src/main/java/uz/pdp/Test.java
+++ src/main/java/uz/pdp/Test.java
@@ -1,4 +1,22 @@
 package uz.pdp;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(addDigits(19));
+    }
+    public static int addDigits(int num) {
+        int res= sumNumbers(num);
+        while(res>9){
+            res =sumNumbers(res);
+        }
+        return res;
+    }
+    public static int sumNumbers(int num){
+        int sum = 0;
+        while(num>0){
+            sum+=num%10;
+            num=num/10;
+        }
+        return sum;
+    }
 }
